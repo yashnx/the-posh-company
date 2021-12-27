6f8b07c11475fd8b151e81b14f1927d1935a28d6
yashnx <85392091+yashnx@users.noreply.github.com>
2021-12-27 22:18:42 +0530

    Update settings.py

--- mac/settings.py
+++ mac/settings.py
@@ -78,8 +78,12 @@ WSGI_APPLICATION = 'mac.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'NAME_OF_DB',
+        'USER': 'DB_USER_NAME',
+        'PASSWORD': 'DB_PASSWORD',
+        'HOST': 'the-posh-company.herokuapp.com/',
+        'PORT': 'PORT_NUMBER',
     }
 }
 
